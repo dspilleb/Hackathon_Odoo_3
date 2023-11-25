0439c7ae74eebbe2fa46b7e34d8fbfd1ad405dc2
Simon <simondefrenne11@gmail.com>
2023-11-25 10:08:12 +0100

    affichage du texte sur fond sur noir

--- scenes/context.py
+++ scenes/context.py
@@ -10,7 +10,7 @@ def activate(display: pygame.Surface, clock: pygame.time.Clock, FPS: int):
 	# scrolling text to explain the game over odoo promotional background music
 
 	# load the background image
-	bg = pygame.image.load('assets/images/bg_filler.png')
+	bg = pygame.image.load('assets/images/ecran-noir.png')
 	# use Arial font
 	font = pygame.font.SysFont('Arial', 25)
 	# create the text cented horizontally
@@ -32,14 +32,14 @@ def activate(display: pygame.Surface, clock: pygame.time.Clock, FPS: int):
 	return
 
 
-def blit_text(surface, text, pos, font, color):
+def blit_text(surface : pygame.Surface, text : str, pos :tuple[int,int] , font : pygame.font.Font, color : tuple[int,int,int]):
     words = [word.split(' ') for word in text.splitlines()]  # 2D array where each row is a list of words.
     space = font.size(' ')[0]  # The width of a space.
-    max_width, max_height = surface.get_size()
+    max_width, max_height = surface.get_size() #size of the window where we display the text
     x, y = pos
     for line in words:
         for word in line:
-            word_surface = font.render(word, 0, color)
+            word_surface = font.render(word, False, color)
             word_width, word_height = word_surface.get_size()
             if x + word_width >= max_width:
                 x = pos[0]  # Reset the x.
--- scenes/office.py
+++ scenes/office.py
@@ -2,6 +2,17 @@ from utils import *
 
 import pygame
 
+
+
+def create_mail(display: pygame.Surface, font: pygame.font.Font, mail_content: str):
+    """
+    Create a mail with the given content and display it on the screen.
+    """
+    mail = pygame.draw.rect(display, Colors.WHITE, pygame.Rect(0, 0, 200, 100))  # Create a rectangle for the mail
+    mail_text = font.render(mail_content, True, Colors.BLACK)  # Render the mail content
+    display.blit(mail_text, (10, 10))  # Display the mail content on the screen
+mails = []
+txt = ""
 #!! so far this just waits for a click
 def activate(display: pygame.Surface, clock: pygame.time.Clock, FPS: int):
 	"""
@@ -9,14 +20,24 @@ def activate(display: pygame.Surface, clock: pygame.time.Clock, FPS: int):
 	
 	# load the background image
 	bg = pygame.image.load('assets/images/bureau.png')
-
+	font = pygame.font.SysFont('Arial', 16)
+	#mail = pygame.image.load('assets/images/mail.png')
+	mail= pygame.draw.rect(display, Colors.WHITE, pygame.Rect(0, 0, 50, 50))
 	while 1:
 		display.blit(bg, (0, 0))
+		create_mail(display, font, 'This is the content of the mail')  # Create a mail
 		upd(clock, FPS)
 		# does nothing until click returns the function
+		player_input = ""
 		for event in pygame.event.get():
 			if event.type == pygame.MOUSEBUTTONDOWN:
 				return
-		
+			#if event.type == pygame.ENT
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_RETURN:
+					print("enter")
+					
+				
 			end()
 		end()
+

214ee9ccddf8cef432cd8cbc7b510b889980ef46
Simon <simondefrenne11@gmail.com>
2023-11-25 17:21:18 +0100

    changement des images et séparation du texte en paragraphes

--- scenes/office.py
+++ scenes/office.py
@@ -1,8 +1,11 @@
 from utils import *
-
 import pygame
 
 
+
+
+def get_user_input(display : pygame.Surface,font : pygame.font.Font):
+	pass
 
 def create_mail(display: pygame.Surface, font: pygame.font.Font, mail_content: str):
     """
@@ -11,6 +14,7 @@ def create_mail(display: pygame.Surface, font: pygame.font.Font, mail_content: s
     mail = pygame.draw.rect(display, Colors.WHITE, pygame.Rect(0, 0, 200, 100))  # Create a rectangle for the mail
     mail_text = font.render(mail_content, True, Colors.BLACK)  # Render the mail content
     display.blit(mail_text, (10, 10))  # Display the mail content on the screen
+
 mails = []
 txt = ""
 #!! so far this just waits for a click
@@ -19,13 +23,14 @@ def activate(display: pygame.Surface, clock: pygame.time.Clock, FPS: int):
 	\nFirst mini-game with the office and emails"""
 	
 	# load the background image
-	bg = pygame.image.load('assets/images/bureau.png')
+	
+	bg = pygame.image.load('assets/images/bureau2.png')
 	font = pygame.font.SysFont('Arial', 16)
 	#mail = pygame.image.load('assets/images/mail.png')
-	mail= pygame.draw.rect(display, Colors.WHITE, pygame.Rect(0, 0, 50, 50))
+	#mail= pygame.draw.rect(display, Colors.WHITE, pygame.Rect(0, 0, 50, 50))
 	while 1:
 		display.blit(bg, (0, 0))
-		create_mail(display, font, 'This is the content of the mail')  # Create a mail
+		#create_mail(display, font, 'This is the content of the mail')  # Create a mail
 		upd(clock, FPS)
 		# does nothing until click returns the function
 		player_input = ""
@@ -36,8 +41,9 @@ def activate(display: pygame.Surface, clock: pygame.time.Clock, FPS: int):
 			if event.type == pygame.KEYDOWN:
 				if event.key == pygame.K_RETURN:
 					print("enter")
-					
-				
+				#user_input = font.render(get_user_input(display,font),True,Colors.GREEN)
+				#print(user_input)
+				#display.blit(user_input, (0, 0))
 			end()
 		end()
 

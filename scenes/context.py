6e97eca74d0263aee702d5088f898212420135ff
RobinRab <rabinowicz.robin@gmail.com>
2023-11-25 16:23:24 +0100

    added office

--- scenes/context.py
+++ scenes/context.py
@@ -10,7 +10,7 @@ def activate(display: pygame.Surface, clock: pygame.time.Clock, FPS: int):
 	# scrolling text to explain the game over odoo promotional background music
 
 	# load the background image
-	bg = pygame.image.load('assets/images/bg_filler.png')
+	#bg = pygame.image.load('assets/images/ecran-noir.png')
 	# use Arial font
 	font = pygame.font.SysFont('Arial', 25)
 	# create the text cented horizontally
@@ -18,12 +18,34 @@ def activate(display: pygame.Surface, clock: pygame.time.Clock, FPS: int):
 	
 
 	# scroll the text from up to down
+	text_1 = "Aujourd'hui, marque le début de ton parcours chez Odoo, et comme chaque stagiaire, tu aspires à décrocher une place durable au sein de l'entreprise. Seulement les trois meilleurs auront la chance de transformer ce stage en opportunité concrète. Seras-tu parmi eux ? Pour le savoir, une règle d'or s'impose dès le départ : ne commettre aucune erreur, à commencer par l'arrivée ponctuelle."
+	
 
-	for i in range(HEIGHT//3):
+	for i in range(HEIGHT//2):
 		# blit dessine l'élément text aux coords 0,i
-		display.blit(bg, (0, 0))
-		display.blit(text, (0, i*3))
+		#display.blit(bg, (0, 0))
+		display.fill(Colors.BLACK)
+		#display.blit(text_1, (0, i*3))
+		blit_text(display, text_1, (0, i*2), font,color=Colors.WHITE)
 		upd(clock, FPS)
 		end()
 
 	return
+
+
+def blit_text(surface : pygame.Surface, text : str, pos :tuple[int,int] , font : pygame.font.Font, color : tuple[int,int,int]):
+	words = [word.split(' ') for word in text.splitlines()]  # 2D array where each row is a list of words.
+	space = font.size(' ')[0]  # The width of a space.
+	max_width, max_height = surface.get_size() #size of the window where we display the text
+	x, y = pos
+	for line in words:
+		for word in line:
+			word_surface = font.render(word, False, color)
+			word_width, word_height = word_surface.get_size()
+			if x + word_width >= max_width:
+				x = pos[0]  # Reset the x.
+				y += word_height  # Start on new row.
+			surface.blit(word_surface, (x, y))
+			x += word_width + space
+		x = pos[0]  # Reset the x.
+		y += word_height  # Start on new row.
--- scenes/office.py
+++ scenes/office.py
@@ -2,21 +2,105 @@ from utils import *
 
 import pygame
 
+import datetime as dt
+
+emails = [
+	"Bonjour, voici le premier email à Traiter",
+]
+
+questions = [
+	{
+		"Question": "Qui aime le chocolat le plus?",
+		"Answer": "Lou"
+	}, 
+]
+
+
 #!! so far this just waits for a click
 def activate(display: pygame.Surface, clock: pygame.time.Clock, FPS: int):
 	"""
 	\nFirst mini-game with the office and emails"""
 	
 	# load the background image
-	bg = pygame.image.load('assets/images/bg_office.jpeg')
+	bg = pygame.image.load('assets/images/desktop.png')
+	bg = pygame.transform.smoothscale(bg, (WIDTH, HEIGHT))
+	font = pygame.font.SysFont('Arial', 25)
 
-	while 1:
-		display.blit(bg, (0, 0))
-		upd(clock, FPS)
-		# does nothing until click returns the function
-		for event in pygame.event.get():
-			if event.type == pygame.MOUSEBUTTONDOWN:
-				return
+	times = []
+
+	for email in emails:
+
+		to_type = font.render(email, True, Colors.BLACK)
+
+		current_mail = ""
+		start = dt.datetime.now()
+		for char in email:
+			while 1:
+				stop = False
+				# when the user presses a key, check if it matches the current char
+				# if it does, add the char to the current_mail
+				# No more comment, code:
+
+				for event in pygame.event.get():
+					if event.type == pygame.KEYDOWN:
+						if event.unicode == char:
+							current_mail += char
+							print(current_mail)
+							stop = True
+							break
+
+					# display the text
+					display.blit(bg, (0, 0))
+					display.blit(to_type, (0, 0))
+					text = font.render(current_mail, True, Colors.BLACK)
+					display.blit(text, (200,200))
 		
-			end()
-		end()
+					upd(clock, FPS)
+					
+				if stop:
+					break
+
+		times.append((dt.datetime.now() - start).total_seconds())
+
+	for Q in questions:
+		question = Q["Question"]
+		answer = Q["Answer"]
+
+		question = font.render(question, True, Colors.BLACK)
+
+		current = ""
+
+		start = dt.datetime.now()
+		while 1:
+			stop = False
+			# when the user presses a key, check if it matches the current char
+			# if it does, add the char to the current_mail
+			# No more comment, code:
+
+			for event in pygame.event.get():
+				if event.type == pygame.KEYDOWN:
+					if event.key == pygame.K_BACKSPACE:
+						current = current[:-1]
+
+						if current.lower() == answer.lower():
+							stop = True
+							break
+					else: 
+						current += event.unicode
+
+				# display the text
+				display.blit(bg, (0, 0))
+				display.blit(question, (0, 0))
+				text = font.render(current, True, Colors.BLACK)
+				display.blit(text, (200,200))
+	
+				upd(clock, FPS)
+
+			if stop:
+				break
+
+		times.append((dt.datetime.now() - start).total_seconds())
+
+	return 1000 - sum(times)*3
+
+	print(times)
